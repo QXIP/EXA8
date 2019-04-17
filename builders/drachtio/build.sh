9c5442aa2fc5cc050781c1bbb29bc7645f69e86d
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2019-04-17 17:11:44 +0200

    drachtio testing

---
+++ builders/drachtio/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+INV=1
+OS="xenial"
+ARCH="arm64"
+INTERCEPTION=0
+VERSION_MAJOR="0.8"
+VERSION_MINOR="1"
+PROJECT_NAME="drachtio"
+TMP_DIR="/tmp/$PROJECT_NAME"
+ENV DEBIAN_FRONTEND noninteractive
+VERSION=0.8
+
+echo "Initiating builder..."
+
+mkdir -p ${TMP_DIR}/usr/local
+apt update
+apt-get update -qq && apt-get install -y build-essential \
+		git libssl-dev libtool autoconf automake zlib1g-dev
+
+cd /usr/src
+git clone --depth=50 --branch=develop git://github.com/davehorton/drachtio-server.git && cd drachtio-server
+git submodule update --init --recursive
+./bootstrap.sh
+./autogen.sh
+mkdir build && cd $_
+../configure CPPFLAGS='-DNDEBUG' CXXFLAGS='-O0' --build=aarch64-linux-gnu --host=aarch64-linux-gnu --target=aarch64-linux-gnu
+
+make
+make install exec_prefix=${TMP_DIR} prefix=${TMP_DIR}
+
+if [ $? -eq 0 ]
+then
+    echo "Proceeding to packaging..."
+else
+    echo "Failed! Exiting..."
+    exit 1;
+fi
+
+apt-get -y install ruby ruby-dev rubygems
+gem install --no-ri --no-rdoc fpm
+
+fpm -s dir -t deb -C ${TMP_DIR} \
+	--name ${PROJECT_NAME} --version ${VERSION_MAJOR}  -p "${PROJECT_NAME}_${VERSION_MAJOR}-${INV}.${OS}.${ARCH}.deb" \
+	--iteration 1 --deb-no-default-config-files --description ${PROJECT_NAME} .
+
+ls -alF *.deb
+cp -v *.deb /scripts/
+
+
