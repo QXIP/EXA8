d6e2f97be1db500b7c32fe42b15e3b92632b9108
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2019-04-21 00:03:25 +0200

    testing influxdb

---
+++ builders/influxdb/build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+GO="1.11"
+INV=1
+OS="xenial"
+ARCH="arm64"
+INTERCEPTION=0
+VERSION_MAJOR="1.0"
+VERSION_MINOR="0"
+PROJECT_NAME="influxdb"
+TMP_DIR="/tmp/$PROJECT_NAME"
+
+echo "Initiating builder..."
+
+
+apt-get update
+apt-get install -y golang-$GO git curl libpcap-dev
+
+export GOARCH=arm64
+export GOROOT="/usr/lib/go-$GO"
+export PATH=$PATH:$GOROOT/bin
+export GO111MODULE=on
+go version
+
+echo "Initiating compiler..."
+cd /usr/src
+git clone https://github.com/influxdata/influxdb
+cd influxdb
+#go get -d -v ./...
+#CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -a -o pcap_exporter -v
+#go build
+
+make
+
+chmod +x influxd
+chmod +x influx
+
+if [ $? -eq 0 ]
+then
+    echo "Proceeding to packaging..."
+	mkdir -p $TMP_DIR/usr/bin
+	cp influxd $TMP_DIR/usr/bin
+	cp influx $TMP_DIR/usr/bin
+else
+    echo "Failed! Exiting..."
+    exit 1;
+fi
+
+apt-get -y install ruby ruby-dev rubygems build-essential
+gem install --no-ri --no-rdoc fpm
+
+fpm -s dir -t deb -C ${TMP_DIR} \
+	--name ${PROJECT_NAME} --version ${VERSION_MAJOR}  -p "${PROJECT_NAME}_${VERSION_MAJOR}-${INV}.${OS}.${ARCH}.deb" \
+	--iteration 1 --deb-no-default-config-files --description ${PROJECT_NAME} .
+
+ls -alF *.deb
+cp -v *.deb /scripts
+
+
---
+++ builders/influxdb/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get install -y qemu-user-static qemu-user
+
+#ARM64 (aarch64)
+docker run -ti --rm --name go-influxdb -v $(pwd)/qemu-aarch64-static:/usr/bin/qemu-aarch64-static -v $(pwd)/:/tmp/build -v $(pwd)/:/scripts --entrypoint=/scripts/build.sh arm64v8/ubuntu:xenial
+
